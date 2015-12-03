7d39abf9aade48196d0934ec4bd5c7a6285b793b
Rafael Belo <rafaelrpbelo@gmail.com>
2015-12-03 00:35:56 -0300

    Change the program name

--- parser.rb
+++ parser.rb
@@ -19,8 +19,8 @@
 # EXPRESSÃO   -> num | num + EXPRESSÃO | num – EXPRESSÃOESSÃO
 
 
+class Main
   attr_reader :tokens
-class Parser
 
   def initialize(tokens_params, options = {})
     if ARGV[0].nil? || ARGV[0].empty?
@@ -201,7 +201,6 @@ exemplo_iterativo = <<-TEXT
 TEXT
 
 puts "\n>>> Init program <<<\n"
-program = Main.new(exemplo_iterativo, debug: true)
 program = Parser.new(exemplo_iterativo)
 puts
 program.process
